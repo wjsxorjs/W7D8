09c4dd6e40a7d0d0273ad16e6c6bcb73a6a6155b
wjsxorjs <wjsxorjs@naver.com>
2024-07-22 12:32:29 +0900

    20240722 Spring 0.5

---
+++ Ex20240722_Kakao/src/main/java/mybatis/vo/MemberVO.java
@@ -0,0 +1,56 @@
+package mybatis.vo;
+
+public class MemberVO {
+	private String m_id, m_name, nickName, p_img, thum_img;
+	private String email, phone, addr;
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getP_img() {
+		return p_img;
+	}
+	public void setP_img(String p_img) {
+		this.p_img = p_img;
+	}
+	public String getThum_img() {
+		return thum_img;
+	}
+	public void setThum_img(String thum_img) {
+		this.thum_img = thum_img;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
